911d45995a54fd22646cd35e6c210f842e51445c
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-10-25 17:44:13 +0200

    reset meteor project

--- src/client/router.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- * Contains global router configurations.
- */
-/* globals Router */
-
-Router.configure({
-  layoutTemplate: 'layoutMain'
-});
--- src/public/images/README.md
+++
@@ -1,2 +0,0 @@
-### Images Folder
-Stick all of your publically acessable images in this folder. :
