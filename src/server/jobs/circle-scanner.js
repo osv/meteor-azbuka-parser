74253fe8da9bad017265b0eea9da4ef11c91c5a0
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2016-03-24 22:26:12 +0200

    minor, move fn

--- src/server/jobs/circle-scanner.js
+++ src/server/jobs/circle-scanner.js
@@ -1,6 +1,6 @@
 /*global CircleJobs, Job, getSettings, console */
 
-Meteor.startup(function() { 
+Meteor.startup(function() {
   var later = CircleJobs.later;
   var queue;
 
@@ -23,56 +23,55 @@ Meteor.startup(function() {
     queue = CircleJobs.processJobs('runScanner', {
       workTimeout: 30 * 60 * 1000,
     }, worker);
-
-    function worker(job, callback) {
-      // Only called when there is a valid job
-      console.log('processing job', job.doc._id);
-      var completedAccum = 0;
-
-      var setting = getSettings(),
-          gender = setting.sex,
-          scrapOptions = setting.scrap,
-          isEnabled = scrapOptions.enable,
-          lastScanDate = scrapOptions.date || new Date(),
-          now = new Date(),
-          diffDays = (now - lastScanDate) / (24 * 60 * 60 * 1000),
-          extraDays = scrapOptions.extraDays,
-          daysForScan = Math.floor(diffDays + extraDays +1);
-
-      if (!isEnabled) {
-        job.done('Scrap is not enabled, skip');
-      }
-
-      if (daysForScan < 1) {
-        job.done('Looks job planned in future: ' + Math.abs(daysForScan));
-      }
-
-      try {
-        if (gender === 'any' || gender === 'female') {
-          process(daysForScan, 'female', logProgressFn);
-        }
-        if (gender === 'any' || gender === 'female') {
-          process(daysForScan, 'male', logProgressFn);
-        }
-      } catch (e) {
-        job.fail(e);
-      } finally {
-        job.done();
-      }
-      
-      console.log('finish job', job.doc._id);
-
-      callback();
-      return;
-
-      // help funtion for progressing job
-      function logProgressFn(numOfProfiles) {
-        completedAccum = completedAccum + numOfProfiles;
-        job.progress(completedAccum, completedAccum + 1);
-      }
-
-    }
   };
+
+  function worker(job, callback) {
+    // Only called when there is a valid job
+    console.log('processing job', job.doc._id);
+    var completedAccum = 0;
+
+    var setting = getSettings(),
+        gender = setting.sex,
+        scrapOptions = setting.scrap,
+        isEnabled = scrapOptions.enable,
+        lastScanDate = scrapOptions.date || new Date(),
+        now = new Date(),
+        diffDays = (now - lastScanDate) / (24 * 60 * 60 * 1000),
+        extraDays = scrapOptions.extraDays,
+        daysForScan = Math.floor(diffDays + extraDays + 1);
+
+    if (!isEnabled) {
+      job.done('Scrap is not enabled, skip');
+    }
+
+    if (daysForScan < 1) {
+      job.done('Looks job planned in future: ' + Math.abs(daysForScan));
+    }
+
+    try {
+      if (gender === 'any' || gender === 'female') {
+        process(daysForScan, 'female', logProgressFn);
+      }
+      if (gender === 'any' || gender === 'female') {
+        process(daysForScan, 'male', logProgressFn);
+      }
+    } catch (e) {
+      job.fail(e);
+    } finally {
+      job.done();
+    }
+
+    console.log('finish job', job.doc._id);
+
+    callback();
+    return;
+
+    // help funtion for progressing job
+    function logProgressFn(numOfProfiles) {
+      completedAccum = completedAccum + numOfProfiles;
+      job.progress(completedAccum, completedAccum + 1);
+    }
+  } // worker
 
   function process(days, sex, progressFn) {
     console.log('process', sex);
@@ -80,13 +79,13 @@ Meteor.startup(function() {
     var page = 1;
     var result = [];
     do {
-//      result = Azbuka.search({days: days, page: page, sex: sex});
+      //      result = Azbuka.search({days: days, page: page, sex: sex});
       page++;
       Meteor._sleepForMs(2000);
       progressFn(3);
 
     } while(result.length);
-    
+
   }
 });
 
